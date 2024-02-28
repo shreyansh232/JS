ff5d779fedd5a686a085db8d023251f08c7068d6
Shreyansh Pandey <shreyansh487@gmail.com>
2024-02-28 10:29:06 +0000

    New

---
+++ .devcontainer/01_basics/05nums.js
@@ -0,0 +1,30 @@
+ const score = 699;
+ console.log(score);
+
+ const balance = new Number(100);
+ console.log(balance);
+ console.log(balance.toString().length);
+ console.log(balance.toFixed(2));
+
+ const otherNumber = 1855.343;
+ console.log(otherNumber.toPrecision(3));
+
+ const hundreds = 1000000;
+ console.log(hundreds.toLocaleString('en-IN'));
+
+
+ //Maths
+ console.log(Math);
+ console.log(Math.round(4.8));
+ console.log(Math.ceil(2.3));
+ console.log(Math.floor(4.6));
+
+ console.log(Math.random());
+ console.log(Math.random()*10);
+ console.log(Math.floor(Math.random()*10)+1);
+
+ const max = 30;
+ const min = 10;
+
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
