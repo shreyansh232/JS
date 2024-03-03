d0fac5fb923c486cbceff8ab11aee7c8d5ded0c3
Shreyansh Pandey <shreyansh487@gmail.com>
2024-03-03 15:51:15 +0000

    new

--- .devcontainer/03_basics/01scope.js
+++ .devcontainer/03_basics/01scope.js
@@ -15,7 +15,25 @@ if(true){
         const website = " Github";
         console.log(name + website);
     }
-    console.log(website);
+    // console.log(website);
 }
 
-console.log(name);
+// console.log(name);
+
+
+
+
+console.log(addOne(5));// This would work
+function addOne(num){
+    return num + 1;
+
+}
+// addOne(5);
+
+console.log(addTwo(5));// This won't work as function is declared in an expression
+const addTwo = function(num){
+    return num + 2;
+
+}
+
+// addTwo(5);
